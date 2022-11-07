c487cb330bfe36d7067464df7c0595f4e5f7dead
aslief <asliefdst1@gmail.com>
2022-11-07 09:23:47 +0700

    first commit

---
+++ app/Http/Controllers/MasterKategori.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MasterKategori;
+use Illuminate\Http\Request;
+
+class MasterKategori extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MasterKategori  $masterKategori
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MasterKategori $masterKategori)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MasterKategori  $masterKategori
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MasterKategori $masterKategori)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MasterKategori  $masterKategori
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MasterKategori $masterKategori)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MasterKategori  $masterKategori
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MasterKategori $masterKategori)
+    {
+        //
+    }
+}
